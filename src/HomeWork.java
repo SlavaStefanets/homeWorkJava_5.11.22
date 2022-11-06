f387c7cab536e3909196510c839d7ac1e865ffbb
slavastefanets <jesusbigboss12@gmail.com>
2022-11-06 13:27:03 +0100

    HomeWorkJava_5.11.22

---
+++ src/HomeWork.java
@@ -0,0 +1,14 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        long chinesePeople = 1_411_778_7241l;
+        int worldsContrys = 197;
+        System.out.println(chinesePeople * worldsContrys);
+
+    long felixLived = 3_011_456_567l; //second
+        felixLived /= 60;
+        felixLived /= 60;
+        felixLived /= 24;
+        felixLived /= 365;
+        System.out.println(felixLived);
+    }
+}
